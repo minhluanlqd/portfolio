a9f0b0d2bf29ffc0924826f6e9e76fc47e0d5066
Luan Tran <minhluanlqd@gmail.com>
2020-07-10 20:54:33 -0400

    contact page

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import {Grid, Cell} from 'react-mdl';
+import avatar from '../../assets/images/MyAvatar.png';
 
 class Contact extends Component{
 
     render(){
         return(
-            <div>
-                Contact Page
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Luan Tran</h2>
+                        <img src={avatar} alt="My avatar" className="avatar-img"/>
+                        <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>Rising Senior at Rutgers University</p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+                        
+                    </Cell>
+                </Grid>
             </div>
         )
     }
