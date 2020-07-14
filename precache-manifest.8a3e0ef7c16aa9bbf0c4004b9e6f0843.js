c6f352b5ee61079a74210b34a8d280d99c483e44
Luan Tran <minhluanlqd@gmail.com>
2020-07-13 23:08:24 -0400

    Updates

---
+++ precache-manifest.8a3e0ef7c16aa9bbf0c4004b9e6f0843.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d285b85306cdc7c5ab5a0775e7695b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c887fc2a993b0e87609",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "345dbf5c5530ac375a41",
+    "url": "/portfolio/static/css/3.0fd4d61c.chunk.css"
+  },
+  {
+    "revision": "2590a3e90246e3087af6",
+    "url": "/portfolio/static/css/main.4c1f4712.chunk.css"
+  },
+  {
+    "revision": "7c887fc2a993b0e87609",
+    "url": "/portfolio/static/js/2.d04e455d.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.d04e455d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345dbf5c5530ac375a41",
+    "url": "/portfolio/static/js/3.c0fabef2.chunk.js"
+  },
+  {
+    "revision": "36477b135b49788015eb84fb70d80461",
+    "url": "/portfolio/static/js/3.c0fabef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36652beb0dd68faee4fe",
+    "url": "/portfolio/static/js/4.5483b416.chunk.js"
+  },
+  {
+    "revision": "6bc624ce74aa6ec9f1c0",
+    "url": "/portfolio/static/js/5.d2c6a5c4.chunk.js"
+  },
+  {
+    "revision": "dff048f80b8d3a39381b",
+    "url": "/portfolio/static/js/6.1c20e598.chunk.js"
+  },
+  {
+    "revision": "41f3da95f6696d3a5598",
+    "url": "/portfolio/static/js/7.6cd083c3.chunk.js"
+  },
+  {
+    "revision": "2590a3e90246e3087af6",
+    "url": "/portfolio/static/js/main.2d2b0101.chunk.js"
+  },
+  {
+    "revision": "c2d5d1020907cc356798",
+    "url": "/portfolio/static/js/runtime-main.56439a99.js"
+  },
+  {
+    "revision": "c433a08707d744ad5b64fcd2f787b920",
+    "url": "/portfolio/static/media/MyAvatar.c433a087.png"
+  },
+  {
+    "revision": "8f89b29b06862a1ebf789d19baa355fe",
+    "url": "/portfolio/static/media/Resume.8f89b29b.pdf"
+  },
+  {
+    "revision": "f5dbd5a57ab24d02cc76b6cbe98a22e9",
+    "url": "/portfolio/static/media/RutgersMenu.f5dbd5a5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3e6f09a964c52b2e12c8f910637d6e3.js"
+  "/portfolio/precache-manifest.8a3e0ef7c16aa9bbf0c4004b9e6f0843.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
