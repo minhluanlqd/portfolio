0f06109bfba30535e47ec1be3e9492937248f06f
Luan Tran <minhluanlqd@gmail.com>
2020-07-13 22:40:37 -0400

    Updates

---
+++ static/js/6.1c20e598.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppersonalwebsite=this.webpackJsonppersonalwebsite||[]).push([[6],{148:function(e,t,n){"use strict";n.r(t);var a=n(15),r=n(16),s=n(18),u=n(17),c=n(0),i=n.n(c),o=function(e){Object(s.a)(n,e);var t=Object(u.a)(n);function n(){return Object(a.a)(this,n),t.apply(this,arguments)}return Object(r.a)(n,[{key:"render",value:function(){return i.a.createElement("div",null,"About Me Page")}}]),n}(c.Component);t.default=o}}]);
+//# sourceMappingURL=6.1c20e598.chunk.js.map
