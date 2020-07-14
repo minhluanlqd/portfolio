40c4946a3493d05d0b9bdbf6751857b224ab1370
Luan Tran <minhluanlqd@gmail.com>
2020-07-13 22:11:11 -0400

    lazy loading

--- src/components/main.js
+++ src/components/main.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import LandingPage from '../components/LandingPage/landingpage';
-import Contact from '../components/Contact/contact';
-import AboutMe from '../components/AboutMe/aboutme';
-import Projects from '../components/Projects/projects';
-import Resume from '../components/Resume/resume';
+
+const Contact = React.lazy(() => import('../components/Contact/contact'));
+const AboutMe = React.lazy(() => import('../components/AboutMe/aboutme'));
+const Projects = React.lazy(() => import('../components/Projects/projects'));
+const Resume = React.lazy(() => import('../components/Resume/resume'))
 
 const Main = () => (
     <Switch>
         <Route exact path="/" component={LandingPage}/>
-        <Route path="/aboutme" component={AboutMe}/>
-        <Route path="/contact" component={Contact}/>
-        <Route path="/resume" component={Resume}/>
-        <Route path="/projects" component={Projects}/>
+        <Route 
+            path="/aboutme" 
+            render={() => (
+                <Suspense fallback={<div>Loading ...</div>}>
+                <AboutMe/>
+                </Suspense>
+            )}
+        />
+        <Route 
+            path="/contact" 
+            render={() => (
+                <Suspense fallback={<div>Loading ...</div>}>
+                <Contact/>
+                </Suspense>
+            )}
+        />
+        <Route 
+            path="/resume" 
+            render={() => (
+                <Suspense fallback={<div>Loading ...</div>}>
+                <Resume/>
+                </Suspense>
+            )}
+        />
+        <Route 
+            path="/projects" 
+            render={() => (
+                <Suspense fallback={<div>Loading ...</div>}>
+                <Projects/>
+                </Suspense>
+            )}
+        />
     </Switch>
 )
 
